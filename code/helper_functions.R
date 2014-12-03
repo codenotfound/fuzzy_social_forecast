6470231d634ab383c09cea8d4d47cbfc71f98c57
codenotfound <dmitry.velikij@gmail.com>
2014-12-03 22:13:35 +0300

    added matching of uneven time series

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -4,6 +4,7 @@ library(stringi)
 library(xlsx)
 library(pander)
 library(reshape)
+library(zoo)
 
 ## load file
 load_xlsx <- function(fname){
@@ -12,7 +13,7 @@ load_xlsx <- function(fname){
     return(df)
 }
 # cleaning
-clean_xslx_df <- function(df){
+clean_xlsx_df <- function(df){
     #Sys.setlocale("LC_TIME", "ru_RU.utf8") 
     #clean header
     names(df) <- stri_trans_general(names(df), 
@@ -24,4 +25,17 @@ clean_xslx_df <- function(df){
     #clean variables
     df$vrema <- as.integer(df$vrema <- substr(df$vrema, start=0, stop=4))
     return(df)
+}
+
+join_factors <- function(factors){
+    # gets list of factors (created by "factors<-list(f1=f1,f2=f2..."
+    # returns zoo onject with filled in NAs
+    z <- zoo()
+    for(factor in factors){
+        z <- merge(z, read.zoo(factor))
+
+    }
+    names(z) <- names(factors)
+    return(z)
+
 }
