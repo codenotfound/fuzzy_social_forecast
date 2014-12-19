667de115b49f70e1a20c38bf139202b502582b7b
codenotfound <dmitry.velikij@gmail.com>
2014-12-20 02:16:56 +0300

    several improvements in functions, error evaluation needs work

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -6,6 +6,7 @@ library(pander)
 library(reshape)
 library(zoo)
 library(ggplot2)
+library(stringr)
 
 ## load file
 load_xlsx <- function(fname){
@@ -53,6 +54,7 @@ fuzzy_forecast <- function(zdf, predictand, include = TRUE, horizon = 3,
                                           name="sim-0")) 
 {
     ## zdf is a zoo object that contains 1..n predictors and exactly one predictand
+    ## include - whether to include predicand as one of the predictors or not
     ## horizon - number of points to forecast
     start <- 1
     finish <- length(zdf[,1])
@@ -75,15 +77,64 @@ fuzzy_forecast <- function(zdf, predictand, include = TRUE, horizon = 3,
     ## Predicting step
     res.test <- predict(object, data.tst)
 
-    model_data <- list(object=object, res.fit=res.fit, res.test=res.test)
+    model_data <- list(object=object, res.fit=res.fit, res.test=res.test, 
+                       zoo_predictand = zdf[,predictand], 
+                       start=start, finish=finish,horizon=horizon)
     return(model_data)
 }
 
-plot_model_data <- function(model_data, predictand)
+plot_model_data <- function(model_data)
 {
     ## Comparing between simulation and real data
     predicted <- rbind(model_data$res.fit, model_data$res.test)
-    df <-data.frame(year=index(predictand),real=coredata(predictand),predicted=predicted) 
+    df <- data.frame(year=index(model_data$zoo_predictand),
+                     real=coredata(model_data$zoo_predictand), predicted=predicted) 
     qplot(year, value, colour = variable, data = melt(df, 'year'), geom = 'line') +
     geom_vline(xintercept=df$year[length(model_data$res.fit)]) + theme_bw()
+}
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  require(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                    ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+ if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
 }
